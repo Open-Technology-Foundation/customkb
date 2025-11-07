ad2db8f1e3ec4edf895c0f4d1e5e86ddf4dcd35b
Biksu Okusi <biksu@okusi.id>
2025-11-07 11:51:40 +0800

    update 1.0.0

--- version.py
+++ version.py
@@ -1,17 +1,17 @@
 """Version information for CustomKB."""
 
 # Version information
-VERSION_MAJOR = 0
-VERSION_MINOR = 9
+VERSION_MAJOR = 1
+VERSION_MINOR = 0
 VERSION_PATCH = 0
-VERSION_BUILD = 13
+VERSION_BUILD = 1
 
 # Formatted version string
 VERSION = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
 VERSION_FULL = f"{VERSION}.{VERSION_BUILD}"
 
 # Release info
-RELEASE_DATE = "2025-09-11"
+RELEASE_DATE = "2025-11-07"
 
 def get_version(build=False):
   """Return version string with optional build number."""
